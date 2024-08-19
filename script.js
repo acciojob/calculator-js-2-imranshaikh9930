92644c412e56ade9e8a829302b0411a4487f44d1
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-08-19 16:54:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+
+const input = document.getElementById("display");
+const buttons =  document.querySelectorAll(".buttons button");
+
+buttons.forEach((btn)=>{
+
+	btn.addEventListener("click",()=>{
+		const btnId = btn.id;
+		
+		// console.log(btnId);
+
+		if(btnId === 'C'){
+			input.value = "";
+		}else if(btnId === "back"){
+			input.value = input.value.slice(0,-1);
+		}else if(btnId === "equal"){
+			try{
+			input.value = eval(input.value);
+			}catch{
+				input.value = "Error"
+			}
+		}else{
+			input.value += btn.textContent;
+		}
+	})
+})
